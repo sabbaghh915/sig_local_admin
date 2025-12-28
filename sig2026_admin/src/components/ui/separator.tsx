2102237b218cb0ad6cdae52255b7b5faf146d39f
sabbaghh915 <sabbaghh915@gmail.com>
2025-12-28 23:23:29 +0300

    delete old file

--- sig2026_admin/src/components/ui/separator.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-function cn(...classes: Array<string | undefined | false | null>) {
-  return classes.filter(Boolean).join(" ");
-}
-
-export function Separator({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn("h-px w-full bg-gray-200", className)} {...props} />;
-}
